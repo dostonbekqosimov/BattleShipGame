7e89e484d9b7b026345d6513e8559861e9c917de
Doston Sulaymon <dostonqosimiy19@gmail.com>
2024-03-29 10:59:58 +0500

    Updated version

--- src/main/java/org/musab/game/DotCom.java
+++ src/main/java/org/musab/game/DotCom.java
@@ -1,5 +1,33 @@
 package org.musab.game;
 
-class DotCom{
+import java.util.ArrayList;
 
+class DotCom {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setLocationCells(ArrayList<String> locationCells) {
+        this.locationCells = locationCells;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("Wow! You sunk " + name + "  : ( ");
+            } else {
+                result = "hit";
+            }
+        }
+        return result;
+
+    }
 }
